6c73218864a0b6915e3e620984ca9695c3c7ebbd
PingMasternt <rovigliofederico@gmail.com>
2022-10-20 17:52:09 -0300

    Ver 1_0

---
+++ PenalJava/src/App.java
@@ -0,0 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args){
+        Logo();
+
+
+    }
+
+
+
+
+
+    public static void Logo(){
+        System.out.println(" _______                                __                      ");
+        System.out.println("/       |                              /  |                     ");
+        System.out.println("$$$$$$$  | ______   _______    ______  $$ |                     ");
+        System.out.println("$$ |__$$ ||      | |       |  |      | $$ |                     ");
+        System.out.println("$$    $$//$$$$$$  |$$$$$$$  | $$$$$$  |$$ |                     ");
+        System.out.println("$$$$$$$/ $$    $$ |$$ |  $$ | /    $$ |$$ |                     ");
+        System.out.println("$$ |     $$$$$$$$/ $$ |  $$ |/$$$$$$$ |$$ |                     ");
+        System.out.println("$$ |     $$       |$$ |  $$ |$$    $$ |$$ |                     ");
+        System.out.println("$$/       $$$$$$$/ $$/   $$/  $$$$$$$/ $$/                      ");
+        System.out.println("                            _____                               ");
+        System.out.println("                           /     |                              ");
+        System.out.println("                           $$$$$ |  ______   __     __  ______  ");
+        System.out.println("                              $$ | /      | /  |   |  |/      | ");
+        System.out.println("                         __   $$ | $$$$$$  |$$  | |$$| $$$$$$  |");
+        System.out.println("                        /  |  $$ | /    $$ | $$  |$$|  /    $$ |");
+        System.out.println("                        $$ |__$$ |/$$$$$$$ |  $$ $$|  /$$$$$$$ |");
+        System.out.println("                        $$    $$| $$    $$ |   $$$|   $$    $$ |");
+        System.out.println("                         $$$$$$|   $$$$$$$|     $|     $$$$$$$| ");
+    }
+
+
+}
