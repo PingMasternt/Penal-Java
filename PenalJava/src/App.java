c4c3b72e41c96045292092c73975213240dc1378
PingMasternt <112631460+PingMasternt@users.noreply.github.com>
2022-10-22 09:53:53 -0300

    Ver 2_0

--- PenalJava/src/App.java
+++ PenalJava/src/App.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args){
         
-        Logo();
-
+        Logo();//Llamada a la funcion "Logo"
+        menuPrincipal();//LLamada a la funcion "menuPrincipal"
 
     }
 
-    public static int[] select = new int[7];
+    public static int[] select = new int[7];//Variable utilizada para interactuar con los menus.
     public static int d20;
 
-    public static void Logo(){
+    public static void Logo(){//Utilizado para mostrar arte ascii
         System.out.println(" _______                                __                      ");
         System.out.println("/       |                              /  |                     ");
         System.out.println("$$$$$$$  | ______   _______    ______  $$ |                     ");
@@ -33,8 +33,21 @@ public class App {
         System.out.println("                         $$$$$$|   $$$$$$$|     $|     $$$$$$$| ");
     }
 
-    public static void menuPrincipal(){
-        System.out.println("");
+    public static void menuPrincipal(){//Menu principal
+        Scanner input = new Scanner(System.in);//Crea un metodo de entrada para el usuario
+        System.out.println("Presione 1) Para empezar \n         2) Para salir");
+        select[0] = input.nextInt();// Asigna un valor al Array Publico select
+        switch(select[0]){
+            case 1:
+            seleccionModalidad();
+            break;
+
+            case 2:
+            break;
+        }
+    }
+    public static void seleccionModalidad(){
+        
     }
 
 }
