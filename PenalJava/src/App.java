561ff8df0044cd300a72e0d718d0938cbf849532
PingMasternt <rovigliofederico@gmail.com>
2022-10-31 17:51:19 -0300

    Ver 3_1

--- PenalJava/src/App.java
+++ PenalJava/src/App.java
@@ -19,6 +19,7 @@ public class App {
     public static int Equipo1, Equipo2;
     public static int PuntuacionJ1;//Almacena la puntuacion del jugador 1
     public static int PuntuacionJ2;//Almacena la puntuacion del jugador 2
+    public static int r = rand.nextInt(10);
 
 
     public static void Logo(){//Utilizado para mostrar arte ascii
@@ -90,22 +91,196 @@ public class App {
     public static void unJugador(){
         cls();
         seleccionDeEquipo();
-        game();
+        cls();
+        for(int i = 0; i < 5; i++){
+            gameJ1();
+        }
+        cls();
+        System.out.println("La puntuacion del Jugador 1 es " + PuntuacionJ1);
+        pause(3000);
+        PuntuacionJ1 = 0;
     }
 
     public static void multijugador(){
         cls();
         seleccionDeEquipo();
+        for(int i = 0; i < 5; i++){
+            gameJ1();
+        }
+        for(int i = 0; i < 5; i++){
+            gameJ2();
+        }
+        System.out.println("La puntuacion del Jugador 1 es " + PuntuacionJ1);
+        System.out.println("La puntuacion del Jugador 2 es " + PuntuacionJ2);
+        if(PuntuacionJ1 > PuntuacionJ2){
+            System.out.println("Gana El jugador 1");
+        }else{
+            System.out.println("Gana El jugador 2");
+        }
+    }
+
+    public static void gameJ1(){//Logica del juego
+        System.out.println("Elige a donde patear la pelota");
+        arco();
+        select[2] = input.nextInt();
+        switch(select[2]){
+            case 1:
+            if(r >= 3){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 2:
+            if(r >= 4){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 3:
+            if(r >= 3){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 4:
+            if(r >= 4){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 5:
+            if(r >= 5){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 6:
+            if(r >= 4){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 7:
+            if(r >= 3){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 8:
+            if(r >= 5){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+            case 9:
+            if(r >= 3){
+                PuntuacionJ1 = PuntuacionJ1 + 1;
+            }else{
+                System.out.println("Fallastes");
+            }
+            break;
+        }
     }
     
+    public static void gameJ2(){//Logica del juego
+         System.out.println("Elige a donde patear la pelota");
+         arco();
+         select[2] = input.nextInt();
+        switch(select[2]){
+             case 1:
+            if(r >= 3){
+                PuntuacionJ2 = PuntuacionJ2 + 1;
+            }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 2:
+                if(r >= 4){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 3:
+                if(r >= 3){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 4:
+                if(r >= 4){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 5:
+                if(r >= 5){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 6:
+                if(r >= 4){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 7:
+                if(r >= 3){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 8:
+                if(r >= 5){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+                case 9:
+                if(r >= 3){
+                    PuntuacionJ2 = PuntuacionJ2 + 1;
+                }else{
+                    System.out.println("Fallastes");
+                }
+                break;
+    
+            }
+        }
+    
+
     public static void cls(){
         for(int i = 0; i <= 20; i++){
             System.out.println(" ");
         }
     }
     
-    public static void game(){//Logica del juego
-        
+    public static void pause(int a){
+        try {
+            Thread.sleep(a);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void arco(){
+        System.out.println(" _______ ");
+        System.out.println("|1  2  3|");
+        System.out.println("|4  5  6|");
+        System.out.println("|7  8  9|");
     }
 }
-
